c11281144b2d9fa766fd7e64c8ade357ef407b45
kieuhuuphuoc <kieuphuoc1412005@gmail.com>
2026-05-05 10:59:18 +0700

    cap nhat

---
+++ app_hoc_tieng_anh/lib/models/topic.dart
@@ -0,0 +1,10 @@
+class Toppic {
+  final int id;
+  final String name;
+  final String description;
+  Toppic({
+    required this.id,
+    required this.name,
+    required this.description,
+  });
+}
---
+++ app_hoc_tieng_anh/lib/models/vocabulary.dart
@@ -0,0 +1,15 @@
+class Vocabulary {
+  final int id;
+  final String word;
+  final String meaning;
+  final String pronunciation;
+  final String examp;
+
+  Vocabulary({
+    required this.id,
+    required this.word,
+    required this.meaning,
+    required this.pronunciation,
+    required this.examp,
+  });
+}
