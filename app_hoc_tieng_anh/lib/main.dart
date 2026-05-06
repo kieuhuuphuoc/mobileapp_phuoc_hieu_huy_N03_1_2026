f6a57f19002ac0a1baf06a7cf69605f8d72360eb
kieuhuuphuoc <kieuphuoc1412005@gmail.com>
2026-05-06 10:40:48 +0700

    Update main.dart

--- app_hoc_tieng_anh/lib/main.dart
+++ app_hoc_tieng_anh/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter UI Demo',
+      title: 'app_hoc_tieng_anh',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
